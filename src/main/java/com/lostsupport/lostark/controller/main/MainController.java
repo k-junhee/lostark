454b94467a06de8dc2133e41c18a32a7df322ca9
junhee <junhee258@naver.com>
2024-03-14 11:12:29 +0900

    변경

---
+++ src/main/java/com/lostsupport/lostark/controller/main/MainController.java
@@ -0,0 +1,15 @@
+package com.lostsupport.lostark.controller.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String main(){
+        return "index";
+    }
+
+}
